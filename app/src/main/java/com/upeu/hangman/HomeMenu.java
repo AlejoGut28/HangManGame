4702c3ef48d708837044fc48449fd4ea2beb877d
Alejandro <alejandrogutierrez@upeu.edu.pe>
2020-09-27 00:28:08 -0500

    terminando el diseño

---
+++ app/src/main/java/com/upeu/hangman/HomeMenu.java
@@ -0,0 +1,14 @@
+package com.upeu.hangman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_menu);
+    }
+}
