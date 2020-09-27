fbb629220d8a645339ac29718b229d9bfcb2dbc2
Alejandro <alejandrogutierrez@upeu.edu.pe>
2020-09-27 18:30:30 -0500

    finalizando el diseño

--- app/src/main/java/com/upeu/hangman/GameOver.java
+++ app/src/main/java/com/upeu/hangman/GameOver.java
@@ -2,14 +2,13 @@ package com.upeu.hangman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class GameInit extends AppCompatActivity {
+public class GameOver extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_init);
+        setContentView(R.layout.activity_game_over);
     }
 }
--- app/src/main/java/com/upeu/hangman/HomeMenu.java
+++ app/src/main/java/com/upeu/hangman/HomeMenu.java
@@ -2,13 +2,24 @@ package com.upeu.hangman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeMenu extends AppCompatActivity {
+    public Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_menu);
+
+        playButton = (Button) findViewById(R.id.playButton);
+    }
+
+    public void changeView(View view) {
+        Intent i = new Intent(this, GameInit.class);
+        startActivity(i);
     }
 }
